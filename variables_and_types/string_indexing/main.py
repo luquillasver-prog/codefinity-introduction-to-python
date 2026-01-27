f6bfb1ed4789fa218645986575d36197e2e13b29
luquillasver-prog <>
2026-01-27 18:06:18 +0000

    String Indexing

---
+++ variables_and_types/string_indexing/main.py
@@ -0,0 +1,10 @@
+grocery_item = "Grilled Chicken Salad"
+lenght_of_item = len(grocery_item)
+
+
+caracter = grocery_item [6]
+caracter2 = grocery_item [-2]
+# Testing
+print("Lenght of the item:"  , len(grocery_item))
+print("Indice del caracter" , caracter)
+print("Indice del caracter2" , caracter2)
