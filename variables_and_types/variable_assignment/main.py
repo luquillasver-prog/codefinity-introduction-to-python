301ed96f5ffb41a4161266e94b51010978f6312e
luquillasver-prog <>
2026-01-27 17:26:29 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,11 @@
+# Definición de variables
+item_name = "Eggs"             # string para el nombre
+item_price = 3.99              # float para el precio
+item_stock_quantity = 120      # integer para cantidad
+in_stock = True            # boolean para disponibilidad
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
